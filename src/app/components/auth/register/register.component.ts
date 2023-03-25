7e00ce7ea2a4c76e16ea878507b2112bc759c86f
lumberjack001 <itoya.shem2017@gmail.com>
2023-03-25 05:10:43 +0100

    adds register page and progress bar

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import countrycodes from '../../../../assets/json/countrycodes.json';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  hide: boolean = false;
+  countries: { countryCode: string; value: number; country: string }[] =
+    countrycodes;
 
+  isHidden() {
+    this.hide = false;
+  }
+  isShown() {
+    this.hide = true;
+  }
 }
+
