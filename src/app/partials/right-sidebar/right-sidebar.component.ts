03a4bc7df6391136b976d573cfd6b9a27caee245
lumberjack001 <itoya.shem2017@gmail.com>
2023-04-15 18:04:54 +0100

    add sidebar carousel

--- src/app/partials/right-sidebar/right-sidebar.component.ts
+++ src/app/partials/right-sidebar/right-sidebar.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-right-sidebar',
   templateUrl: './right-sidebar.component.html',
-  styleUrls: ['./right-sidebar.component.scss']
+  styleUrls: ['./right-sidebar.component.scss'],
 })
 export class RightSidebarComponent {
-
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 5000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = true;
+    config.showNavigationArrows = false;
+    config.showNavigationIndicators = true;
+  }
 }
