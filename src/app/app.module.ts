764f461915eb348e7319a5db240d47796350e757
lumberjack001 <itoya.shem2017@gmail.com>
2023-04-29 13:25:44 +0100

    added blogccomponent to module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,13 +46,13 @@ import { LandingpagefeaturesComponent } from './components/landing-page/landingp
 import { LandingpageCampaignComponent } from './components/landing-page/landingpage-campaign/landingpage-campaign.component';
 import { RadioBtnComponent } from './partials/radio-btn/radio-btn.component';
 import { SubscriptionComponent } from './partials/modals/subscription/subscription.component';
-import { ChatComponent } from './partials/chat/chat.component';
 
 import { HeaderComponent } from './partials/header/header.component';
 import { LeftSidebarComponent } from './partials/left-sidebar/left-sidebar.component';
 import { RightSidebarComponent } from './partials/right-sidebar/right-sidebar.component';
 import { NavIconsComponent } from './partials/nav-icons/nav-icons.component';
 import { SubscriptionCardComponent } from './partials/subscription-card/subscription-card.component';
+import { BlogComponent } from './components/dashboard/blog/blog.component';
 
 @NgModule({
   declarations: [
@@ -73,13 +73,12 @@ import { SubscriptionCardComponent } from './partials/subscription-card/subscrip
     LandingpageCampaignComponent,
     RadioBtnComponent,
     SubscriptionComponent,
-    ChatComponent,
-  
     HeaderComponent,
     LeftSidebarComponent,
     RightSidebarComponent,
     NavIconsComponent,
     SubscriptionCardComponent,
+    BlogComponent,
   ],
   imports: [
     BrowserModule,
