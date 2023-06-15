5502ce8195f34730e7fddc97fcafa43b2d3e7a9b
Eloagbawe <eloagbawe@gmail.com>
2023-06-15 22:22:22 +0100

    update landing page navbar to load logo image based on theme and hide auth buttons if user is logged in

--- src/app/partials/landingpage-navbar/landingpage-navbar.component.ts
+++ src/app/partials/landingpage-navbar/landingpage-navbar.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Router } from '@angular/router';
 
+import {
+  getThemeStart,
+} from '../../store/actions/theme.action'
+
+import {
+  getTheme
+} from '../../store/selector/theme.selector'
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-landingpage-navbar',
   templateUrl: './landingpage-navbar.component.html',
   styleUrls: ['./landingpage-navbar.component.scss']
 })
 export class LandingpageNavbarComponent implements OnInit {
+  theme$!:Observable<any>;
+  theme!:string;
+  loggedIn!: boolean;
+  routeURL!: string;
 
-  constructor() { }
+  constructor(private store$: Store, private router: Router) {
+    this.routeURL = this.router.url;
+  }
 
   ngOnInit(): void {
+    this.store$.dispatch(getThemeStart())
+    this.theme$ = this.store$.select(getTheme);
+
+    this.theme$.subscribe((theme) => {
+      this.theme = theme;
+    })
+
+    this.loggedIn = !!localStorage.getItem('accessToken');
   }
 
 }
