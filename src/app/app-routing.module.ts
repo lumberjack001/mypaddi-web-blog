00e097595c72a6688b8be9667a24789629b5b99a
Eloagbawe <eloagbawe@gmail.com>
2023-06-15 22:11:01 +0100

    add login auth guard to auth routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,9 @@ import { SubscriptionComponent } from './partials/modals/subscription/subscripti
 import { AudioComponent } from './components/dashboard/audio/audio.component';
 import { AlbumDetailsComponent } from './components/dashboard/album-details/album-details.component';
 
+import { AuthGuard } from './guard/auth.guard';
+import { LoginAuthGuard } from './guard/login-auth.guard';
+
 const routes: Routes = [
   {
     path: '',
@@ -21,6 +24,7 @@ const routes: Routes = [
   },
   {
     path: 'auth',
+    canActivate: [LoginAuthGuard],
     component: AuthComponent,
     children: [
       { path: 'login', component: LoginComponent },
